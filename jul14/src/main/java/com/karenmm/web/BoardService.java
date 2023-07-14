0aa7eedaa7e441aa543f9257159ac1df70b43708
GMKOO <karenmm@naver.com>
2023-07-14 16:39:20 +0900

    스프링 4일차

---
+++ jul14/src/main/java/com/karenmm/web/BoardService.java
@@ -0,0 +1,24 @@
+package com.karenmm.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+	
+	@Autowired
+	private BoardDAO boardDAO;
+
+	public List<BoardDTO> boardList() {
+		
+	
+	
+		
+		return boardDAO.boardList();
+	}
+
+
+	
+}
---
+++ jul14/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>인덱스입니다</title>
+</head>
+<body>
+ <h1>스프링 4일차?</h1>
+ 
+ <a href="./main"> main </a>
+
+</body>
+</html>
